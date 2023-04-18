d60bdeae012bf4cadeb41946dbb1c8f38cb92e4d
Shivam <shivamkasaudhan266@gmail.com>
2023-04-18 20:16:04 +0530

    day 2

---
+++ problem3.cpp
@@ -0,0 +1,29 @@
+// Write a program that plays the game of Mad Lib. Your program should 
+// prompt the user to enter the following strings:
+// ■ The first or last name of your instructor
+// ■ Your name
+// ■ A food
+// ■ A number between 100 and 120
+// ■ An adjective
+// ■ A color
+// ■ An animal
+// After the strings are input, they should be substituted into the story below 
+// and output to the console.
+// Solution to Practice 
+// Program 2.3
+// VideoNote
+// Programming Projects 105
+// Dear Instructor [Instructor Name],
+// I am sorry that I am unable to turn in my homework at this time. First, 
+// I ate a rotten [Food], which made me turn [Color] and extremely ill. I 
+// came down with a fever of [Number 100-120]. Next, my [Adjective] pet 
+// [Animal] must have smelled the remains of the [Food] on my homework, 
+// because he ate it. I am currently rewriting my homework and hope you 
+// will accept it late.
+// Sincerely,
+// [Your Name]
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
