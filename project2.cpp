e152cc232ff53a7575f08e7544e0758e34846cd5
Shivam <shivamkasaudhan266@gmail.com>
2023-04-19 22:25:21 +0530

    day 3

---
+++ project2.cpp
@@ -0,0 +1,14 @@
+// Workers at a particular company have won a 7.6% pay increase retroactive 
+// for 6 months. Write a program that takes an employee’s previous annual 
+// salary as input, and outputs the amount of retroactive pay due the employee,
+ //the new annual salary, and the new monthly salary. Use a variable 
+// declaration with the modifier const to express the pay increase. Your program 
+//should allow the calculation to be
+#include <iostream>
+using namespace std;
+int main()
+{
+    const int increse =0.076;
+    
+    return 0;
+}
