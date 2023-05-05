c1268e85dafafce5952d78594a1b01a62fa2cfc0
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-05 09:12:22 +0530

    Day 18

---
+++ arraychallenge.cpp
@@ -0,0 +1,21 @@
+//chalenge
+#include <iostream>
+using namespace std;
+int main(){
+    int mx= -999999999;
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        mx= max(mx,arr[i]);
+        cout<<mx<<endl;
+    }
+    
+    
+    return 0;
+}
