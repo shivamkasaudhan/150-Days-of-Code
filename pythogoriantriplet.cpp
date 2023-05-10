1945059640cc7bab95b0cee0338cd38864ae6841
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-10 11:30:08 +0530

    Day 23

---
+++ pythogoriantriplet.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+bool ckeck(int x, int y, int z){
+    int a = max(x,max(y,z));
+    int b,c;
+    if (a==x){
+        b=y;
+        c=z;
+    }else if (a==y)
+    {
+        b=x;
+        c=z;
+    }else{
+        b=y;
+        c=x;
+    }
+    if (a*a== b*b+c*c)
+    {
+        return true;
+    }
+    return false;
+    
+}
+int main(){
+    int a, b,c;
+    cin>>a>>b>>c;
+    cout<<ckeck(a,b,c);
+    return 0;
+}
