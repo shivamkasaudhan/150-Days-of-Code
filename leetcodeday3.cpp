c1e50c7166b0a9c40a30517ab79be69f8a2d26ba
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-10 20:18:19 +0530

    Day 24

--- leetcodeday3.cpp
+++ leetcodeday3.cpp
@@ -41,3 +41,18 @@ public:
     }
 };
 
+// 771. Jewels and Stones
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count =0;
+        for(int i=0; i<jewels.size();i++ ){
+            for(int j=0;j<stones.size();j++){
+                if(jewels[i]==stones[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
