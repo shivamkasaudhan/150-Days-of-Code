c18f4bbb02493e8578e19be01ab25718b7c7823a
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-11 11:12:35 +0530

    Day 25

--- leetcode6.cpp
+++ leetcode6.cpp
@@ -20,3 +20,16 @@ public:
         return s;
     }
 };
+// 70. Climbing Stairs
+class Solution {
+public:
+    int climbStairs(int n) {
+        int a = 0, b= 1;
+        for(int i = 0; i< n; i++){
+            int temp = a+b;
+            a= b;
+            b= temp;
+        }
+        return b;
+    }
+};
