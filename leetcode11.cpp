342021def1a4743174a88c1fa3d9cacb29b50d4c
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-18 23:58:37 +0530

    Day 32

--- leetcode11.cpp
+++ leetcode11.cpp
@@ -51,3 +51,13 @@ public:
         return ans;
     }
 };
+// 1464. Maximum Product of Two Elements in an Array
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(),nums.end());
+        return (nums[n-1]-1)*(nums[n-2]-1);
+        
+    }
+};
