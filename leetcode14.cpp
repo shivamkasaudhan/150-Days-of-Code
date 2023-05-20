1ce7ee3ac343103f00eb283b63d37ac0a1ca627d
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-20 19:02:20 +0530

    Day 34

--- leetcode14.cpp
+++ leetcode14.cpp
@@ -1,12 +1,12 @@
-// // 169. Majority Element
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         int n= nums.size();
-//         return nums[n/2];   
-//     }
-// };
+// 169. Majority Element
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n= nums.size();
+        return nums[n/2];   
+    }
+};
 #include <iostream>
 using namespace std;
 int main(){
