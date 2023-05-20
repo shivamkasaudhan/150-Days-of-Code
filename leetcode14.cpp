51277da27015af6f05b73f3ddb4eb5349660f27d
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-05-20 19:01:38 +0530

    Day 34

---
+++ leetcode14.cpp
@@ -0,0 +1,18 @@
+// // 169. Majority Element
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int n= nums.size();
+//         return nums[n/2];   
+//     }
+// };
+#include <iostream>
+using namespace std;
+int main(){
+    string n= "shivam";
+    cout<<n[6]<<endl;
+    cout<<n.size();
+
+    return 0;
+}
