6f522d045d9da1c6a36721242026dd92e2d78bd4
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-01 17:30:48 +0530

    Day 46

--- leetcode30.cpp
+++ leetcode30.cpp
@@ -30,3 +30,20 @@ public:
         
     }
 };
+// 1346. Check If N and Its Double Exist
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        bool ans=false;
+        for(int i=0;i<arr.size();i++){
+            for(int j=0;j<arr.size();j++){
+                if((arr[i]==2*arr[j] || 2*arr[i]==arr[j] ) && i!=j){
+                    ans=true;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};
+
