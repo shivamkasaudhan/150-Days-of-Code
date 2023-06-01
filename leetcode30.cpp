98857e806e48ad0a9b220781b5dcccd3784a3b2e
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-01 19:00:20 +0530

    Day 46

--- leetcode30.cpp
+++ leetcode30.cpp
@@ -46,4 +46,22 @@ public:
         
     }
 };
+// 2006. Count Number of Pairs With Absolute Difference K
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(abs(nums[i]-nums[j])==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
+
+
 
