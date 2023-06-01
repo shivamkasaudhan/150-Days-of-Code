d7ccfebcd1fe1a6a2e225f254e3287c0fb6c6cfa
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-01 17:10:22 +0530

    Day 46

--- leetcode30.cpp
+++ leetcode30.cpp
@@ -11,3 +11,22 @@ public:
         
     }
 };
+// 1385. Find the Distance Value Between Two Arrays
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int i,j, ans=0;
+        for( i=0;i<arr1.size();i++){
+            for(j=0;j<arr2.size();j++){
+                int diff =abs(arr1[i]-arr2[j]);
+                if(diff<=d){
+                    break;
+                }
+            }
+            if(j==arr2.size())
+                    ans++;
+        }
+        return ans;
+        
+    }
+};
