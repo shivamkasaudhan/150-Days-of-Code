12c3e564c59d4c30e9c6816c8023301be04d77d8
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-02 13:51:23 +0530

    Day 47

--- leetcode31.cpp
+++ leetcode31.cpp
@@ -13,3 +13,22 @@ public:
         
     }
 };
+// 1342. Number of Steps to Reduce a Number to Zero
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int count =0;
+        while(num>0){
+            if(num%2!=0 ){
+                num-=1; 
+            }else{
+                num/=2;
+            }
+            count++;
+        }
+        return count;
+        
+    }
+};
+
+
