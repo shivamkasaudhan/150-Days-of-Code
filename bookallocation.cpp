54f9526f71f31468313e5b962ae677cba46a815b
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-05 23:11:31 +0530

    Incomplete

---
+++ bookallocation.cpp
@@ -0,0 +1,23 @@
+//codechef
+// Book Allocation Problem
+// You are given an array ‘pages’ of integer numbers. In this array, the ‘pages[i]’ represents the number of pages in the ‘i-th’ book. There are ‘m’ number of students, and the task is to allocate all the books to the students. 
+// Allocate books in a way such that
+// Each student gets at least one book.
+// Each book should be allocated to a student.
+// Book allocation should be in a contiguous manner.
+// You have to allocate the books to ‘m’ students such that the maximum number of pages assigned to a student is minimum.
+// Try to solve this problem on your own before moving on to further discussion here.
+// Let’s understand the problem statement through an example.
+// Number of books = 4 and Number of students = 2
+// pages[] = { 10,20,30,40}
+// Output
+//  60
+#include <iostream>
+using namespace std;
+int main(){
+    int number_of_book=4;
+    int student =2;
+    int Pair[4]={10,20,30,40};
+    
+    return 0;
+}
