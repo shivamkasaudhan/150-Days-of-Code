2c0b585a183b3a4a554dadb2910ed9c00785467a
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-24 00:12:26 +0530

    Day 68

--- leetcode55.cpp
+++ leetcode55.cpp
@@ -1,4 +1,6 @@
-
+// 1812. Determine Color of a Chessboard Square
+//O(n)
+//100% beats
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
@@ -7,3 +9,23 @@ public:
 
     }
 };
+// 389. Find the Difference
+//100% beats
+//tc O(m+n)
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int sum=0;
+        int sum1=0;
+        for(int i=0;i<s.size();i++){
+            sum = sum + s[i];
+        }
+        for(int i=0;i<t.size();i++){
+            sum1 = sum1 + t[i];
+        }
+        char ans= sum1-sum;
+        return ans;
+        
+    }
+};
+
