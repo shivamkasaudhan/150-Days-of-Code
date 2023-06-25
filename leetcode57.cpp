8a5c3a29925d3f56fef38a93952aafe813bcfcfc
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-25 21:39:46 +0530

    Day 70

--- leetcode57.cpp
+++ leetcode57.cpp
@@ -28,3 +28,28 @@ public:
         return count;   
     }
 };
+
+// 551. Student Attendance Record I
+//tc O(n)
+//100% beats
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int count=0;
+        bool flag= true;
+        for(int i=0; i<s.size();i++){
+            if(s[i]=='A'){
+                count++;
+            }
+            if(s[i]=='L' && s[i+1]=='L' && s[i+2]=='L'){
+                flag= false;
+            }
+        }
+        if(count>=2){
+            flag= false;
+        }
+        return flag;
+        
+    }
+}; 
+
