95f99e5bba84c77115977f9a0e80241770c952a0
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-25 17:16:37 +0530

    Day 70

--- leetcode57.cpp
+++ leetcode57.cpp
@@ -10,3 +10,21 @@ public:
         
     }
 };
+// 2586. Count the Number of Vowel Strings in Range
+//tc O(n)
+class Solution {
+public:
+    bool vowelorNot(char c){
+        return c == 'a' || c ==  'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    int vowelStrings(vector<string>& words, int left, int right) {
+        int count=0;
+        for(int i=left;i<=right;i++){
+            string word=words[i];
+            if(vowelorNot(word[0]) && vowelorNot(word[word.size()-1])){
+                count++;
+            } 
+        }
+        return count;   
+    }
+};
