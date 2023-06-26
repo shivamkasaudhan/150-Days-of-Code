7e57e376c9a683643d9c784abe8af822cb2abb7d
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-06-26 18:50:59 +0530

    Day 71

---
+++ leetocde58.cpp
@@ -0,0 +1,38 @@
+// 2299. Strong Password Checker II
+// tc O(n)
+//100 % beats
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        int charcount=0;
+        int lowercase=0;
+        int uppercase=0;
+        int digit=0;
+        int special=0;
+        for(int i=0;i<password.size();i++){
+            if(password[i]>='a' && password[i]<='z'){
+                lowercase++;
+                charcount++;
+            }else if(password[i]>='A' && password[i]<='Z'){
+                uppercase++;
+                charcount++;
+            }else if(password[i]>='0' && password[i]<='9'){
+                digit++;
+                charcount++;
+            }else{
+                special++;
+                charcount++;
+            }
+
+            if(password[i]==password[i+1]){
+                return false;
+            }
+        }
+        if(charcount>=8 && lowercase>=1 && uppercase>=1 && digit>=1 && special>=1){
+            return true;
+        }
+        return false;
+
+        
+    }
+};
