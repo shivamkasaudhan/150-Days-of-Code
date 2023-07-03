c2f1379c4f603b4c7f3f54bd10d0ebcd5d903949
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-07-03 14:03:00 +0530

    Day 78

--- leetcode64.cpp
+++ leetcode64.cpp
@@ -1,1 +1,29 @@
-2643. Row With Maximum Ones
+// 2643. Row With Maximum Ones
+//tc O(n^2)
+class Solution {
+public:
+    vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        vector<int> count;
+        vector<int> ans;
+        for(int i=0;i<mat.size();i++){
+            int n=0;
+            for(int j=0;j<mat[i].size();j++){
+                if(mat[i][j]==1){
+                    n++;
+                }
+            }
+            count.push_back(n);
+        }
+        int max= *max_element(count.begin(),count.end());
+        int index=0;
+        for(int i=0;i<count.size();i++){
+            if(count[i]==max){
+                index=i;
+                break;
+            }
+        }
+        ans.push_back(index);
+        ans.push_back(max);
+        return ans;
+    }
+};
