554e103d2aa8e1477474db23226418ca15e78ff6
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-07-21 23:28:07 +0530

    Day 95

---
+++ leetcode83.cpp
@@ -0,0 +1,42 @@
+// 54. Spiral Matrix
+//tc O(n*m)
+//100% beats
+//code
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int row= matrix.size();
+        int col= matrix[0].size();
+        int count=0;
+        int total= row*col;
+        int startcol=0;
+        int startrow=0;
+        int endrow= row-1;
+        int endcol= col-1;
+        while(count<total){
+            for(int i=startcol;count<total && i<=endcol;i++){
+                ans.push_back(matrix[startrow][i]);
+                count++;
+            }
+            startrow++;
+            for(int i=startrow;count<total && i<=endrow;i++){
+                ans.push_back(matrix[i][endcol]);
+                count++;
+            }
+            endcol--;
+            for(int i=endcol;count<total && i>=startcol;i--){
+                ans.push_back(matrix[endrow][i]);
+                count++;
+            }
+            endrow--;
+            for(int i=endrow;count<total && i>=startrow;i--){
+                ans.push_back(matrix[i][startcol]);
+                count++;
+            }
+            startcol++;
+        }
+        return ans;
+        
+    }
+};
