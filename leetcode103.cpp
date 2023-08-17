8ff9d9bfb159255a28d94a1044e3f10df60c7fa7
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-08-17 14:58:19 +0530

    Day 121

---
+++ leetcode103.cpp
@@ -0,0 +1,14 @@
+// 217. Contains Duplicate
+//tc O(nlogn)
+//sc O(1)
+//code
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]) return true;
+        }
+        return false;
+    }
+};
