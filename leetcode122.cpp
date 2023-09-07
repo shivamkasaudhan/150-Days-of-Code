2f53c2ab1a64e3b3ebd9c24315fedb83f13039ca
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-09-07 23:44:46 +0530

    Day 142

---
+++ leetcode122.cpp
@@ -0,0 +1,30 @@
+// 1582. Special Positions in a Binary Matrix
+//tc O(n*m)
+//sc O(m+n)
+///code
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n= mat[0].size();
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i]==1 && col[j]==1 && mat[i][j]){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
