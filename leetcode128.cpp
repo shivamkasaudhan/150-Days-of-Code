4e05b6b508b391bf19d8239528f0bdad16f22272
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-09-14 00:06:41 +0530

    Day 148

--- leetcode128.cpp
+++ leetcode128.cpp
@@ -1,6 +1,6 @@
 // 8. String to Integer (atoi)
 //tc O(n)
-//sc O(n)
+//sc O(1)
 //code
 
 class Solution {
@@ -33,5 +33,40 @@ public:
         }
         return (int)(sign*ans);
         
+    }
+};
+//optimized and correct code
+class Solution {
+public:
+    bool digit(char s) {
+        if (s >= '0' && s <= '9') {
+            return true;
+        }
+        return false;
+    }
+
+    int myAtoi(string s) {
+        if (s.length() == 0) return 0;
+        int i = 0;
+        while (i < s.size() && s[i] == ' ') {
+            i++;
+        }
+        s = s.substr(i);
+        int sign = 1; 
+        long ans = 0;
+        const int MIN = INT_MIN;
+        const int MAX = INT_MAX;
+
+        if (s[0] == '-') sign = -1;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+
+        while (i < s.length() && digit(s[i])) {
+            if (ans > (MAX - (s[i] - '0')) / 10) {
+                return sign == 1 ? MAX : MIN;
+            }
+            ans = ans * 10 + s[i] - '0';
+            i++;
+        }
+        return (int)(sign * ans);
     }
 };
