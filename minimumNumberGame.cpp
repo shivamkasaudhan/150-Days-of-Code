f819569ebac3cd630b0f67b61ed8bcd4fdfb346b
SHIVAM KUMAR KASAUDHAN <124242999+shivamkasaudhan@users.noreply.github.com>
2023-12-28 23:32:03 +0530

    Leetcode question

---
+++ minimumNumberGame.cpp
@@ -0,0 +1,14 @@
+//2974. Minimum Number Game
+//tc nlog(n)
+//sc O(1)
+//code
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i =0; i<nums.size(); i+=2){
+            swap(nums[i],nums[i+1]);
+        }
+        return nums;
+    }
+};
